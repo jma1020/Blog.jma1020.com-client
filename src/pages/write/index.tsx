793265de2953566616ad39703907b568d1b26523
jeongmin Kim <jma1020@naver.com>
2021-12-30 12:17:37 +0900

    ✨ write에서 글쓰기 추가

--- src/pages/write/index.tsx
+++ src/pages/write/index.tsx
@@ -1,5 +1,21 @@
 import { NextPage } from "next";
 import React, { useState } from "react";
+import axios from 'axios'
+
+const write = (title: string , post: string) => {
+    axios.post('http://localhost:1337/api/posts/',{
+        "data": {
+            "author" : "김쩡",
+            "Title" : title,
+            "Contents" : post,
+        }
+    }).then((res) => {
+        if(res.status===200) {
+            location.href = '/posts'
+            //이런거 라우터로 해도 된다
+        }
+    }) 
+}
 
 const WritePage:NextPage =() => {
     const [ title, setTitle ] =useState('')
@@ -10,13 +26,18 @@ const WritePage:NextPage =() => {
     }
 
     const handleTitleChange = (e:React.ChangeEvent<HTMLInputElement>)=>{
-        setTitle(e.target.value)
+        setTitle(e.target.value) 
+    }
+
+    const handleSubmnit = (e:React.FocusEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        write(title, post)
     }
 
     return(
         <div>
             <h1>글 작성</h1>
-            <form>
+            <form onSubmit={handleSubmnit}>
                 <input type="text" onChange={handleTitleChange} value={title}/>
                 <textarea onChange={handleTextAreaChange} value={post}  />
                 <button type="submit">발행하기 </button>
